8ee79555d4469af989480b319be4f219c9057a6e
sriraj_nsns <nsnsdevilsboy@gmail.com>
2026-02-17 10:25:22 +0530

    UC1: print Hello World message

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
         // This line prints the text to the console
-        System.out.println("OOPS");
+        System.out.println("printing oops");
     }
 }
